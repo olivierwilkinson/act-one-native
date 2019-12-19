606451944cd558abc158eb973a9c10505e9d9e5c
olivierwilkinson <olivier.wilkinson@gmail.com>
2019-12-19 15:20:33 +0000

    Make header primary colour

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { Header, Body, Title } from 'native-base';
+import styled from 'styled-components';
+
+import { primaryColour } from '../styles/colours.js';
+
+const PrimaryHeader = styled(Header)`
+  background-color: ${primaryColour};
+`;
 
 export default function PlayList() {
   return (
-    <Header>
+    <PrimaryHeader>
       <Body>
         <Title>ActOne</Title>
       </Body>
-    </Header>
+    </PrimaryHeader>
   );
 }
