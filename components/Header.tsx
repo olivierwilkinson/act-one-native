3b8d26a017498794415388aaeb64af632c6b4588
olivierwilkinson <olivier.wilkinson@gmail.com>
2019-12-20 16:50:10 +0000

    Update Header to have light styling

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,24 @@
 import React from "react";
-import { Header, Body, Title } from 'native-base';
-import styled from 'styled-components';
+import { Header, Body, Title } from "native-base";
+import styled from "styled-components";
+import { StatusBar } from "react-native";
 
-import { primaryColour } from '../styles/colours.js';
+import { primaryColour } from "../styles/colours.js";
 
 const PrimaryHeader = styled(Header)`
   background-color: ${primaryColour};
 `;
 
+const WhiteTitle = styled(Title)`
+  color: white;
+`;
+
 export default function PlayList() {
   return (
     <PrimaryHeader>
+      <StatusBar barStyle="light-content" />
       <Body>
-        <Title>ActOne</Title>
+        <WhiteTitle>ActOne</WhiteTitle>
       </Body>
     </PrimaryHeader>
   );
