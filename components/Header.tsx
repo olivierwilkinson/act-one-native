22979373fdada0b6a3aff1dba4acccaa8478774c
olivierwilkinson <olivier.wilkinson@gmail.com>
2019-12-24 16:31:05 +0000

    Update Header to accept title and onBack props

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { Header, Body, Title } from "native-base";
+import { Header as BaseHeader, Body, Left, Title } from "native-base";
+import { TouchableHighlight } from "react-native";
 import styled from "styled-components";
 import { StatusBar } from "react-native";
 
 import { primaryColour } from "../styles/colours.js";
 
-const PrimaryHeader = styled(Header)`
+const PrimaryHeader = styled(BaseHeader)`
   background-color: ${primaryColour};
 `;
 
@@ -13,13 +14,31 @@ const WhiteTitle = styled(Title)`
   color: white;
 `;
 
-export default function PlayList() {
-  return (
-    <PrimaryHeader>
-      <StatusBar barStyle="light-content" />
-      <Body>
-        <WhiteTitle>ActOne</WhiteTitle>
-      </Body>
-    </PrimaryHeader>
-  );
-}
+type Props = {
+  title?: string;
+  onBackClick?: () => void;
+};
+
+const defaultProps = {
+  title: "ActOne"
+};
+
+const Header = (props: Props) => (
+  <PrimaryHeader>
+    <StatusBar barStyle="light-content" />
+
+    {props.onBackClick && (
+      <Left>
+        <TouchableHighlight>Back</TouchableHighlight>
+      </Left>
+    )}
+
+    <Body>
+      <WhiteTitle>ActOne</WhiteTitle>
+    </Body>
+  </PrimaryHeader>
+);
+
+Header.defaultProps = defaultProps;
+
+export default Header;
