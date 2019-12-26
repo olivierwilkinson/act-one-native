49fa02df5cf40974f2880e6440cc0dae622cbc7d
olivierwilkinson <olivier.wilkinson@gmail.com>
2019-12-26 15:32:22 +0000

    Format types

--- types/play-types.d.ts
+++ types/play-types.d.ts
@@ -1,26 +1,26 @@
 export interface LineRow {
-  text: string,
-  number?: number,
-};
+  text: string;
+  number?: number;
+}
 
 export interface Line {
-  id: number,
-  actSceneLine: string,
-  player: string,
-  lineRows: LineRow[]
-};
+  id: number;
+  actSceneLine: string;
+  player: string;
+  lineRows: LineRow[];
+}
 
 export interface Scene {
-  act: number,
-  scene: number,
-  lines: Line[],
-};
+  act: number;
+  scene: number;
+  lines: Line[];
+}
 
 export interface Play {
   play: string;
   description: string;
   script: Scene[];
-  image: string,
-  imageLicenseUrl: string,
-  imageLicenseCode: string,
-};
+  image: string;
+  imageLicenseUrl: string;
+  imageLicenseCode: string;
+}
