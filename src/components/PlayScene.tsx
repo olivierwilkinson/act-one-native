23519bcc9729ed7591ef1b594180a4ad8f71604f
Olivier Wilkinson <olivier.wilkinson@gmail.com>
2019-12-27 14:39:05 +0000

    Feature/paginated plays (#2)
    * Update Play to render one scene at a time

--- src/components/PlayScene.tsx
+++ src/components/PlayScene.tsx
@@ -1,24 +1,32 @@
-import React from "react";
+import React, { forwardRef, Component } from "react";
 import { SectionList } from "react-native";
 
 import PlayLine from "./PlayLine";
 import PlayLineHeader from "./PlayLineHeader";
-import { Scene } from "../types/play-types";
+import { Scene, Line } from "../types/play-types";
 
 type Props = Scene & {
   colourByPlayer: { [player: string]: number[] };
 };
 
-export default ({ lines, colourByPlayer }: Props) => (
-  <SectionList
-    sections={lines.map(line => ({
-      data: [line],
-      title: line.player
-    }))}
-    renderItem={({ item: line }) => <PlayLine {...line} />}
-    renderSectionHeader={({ section: { title: player } }) => (
-      <PlayLineHeader player={player} colour={colourByPlayer[player]} />
-    )}
-    keyExtractor={item => item.id.toString()}
-  />
+type ListData = {
+  data: Line[];
+  title: string;
+}[];
+
+export default forwardRef<Component<{ sections: ListData }>, Props>(
+  ({ lines, colourByPlayer }: Props, ref) => (
+    <SectionList
+      ref={ref}
+      sections={lines.map(line => ({
+        data: [line],
+        title: line.player
+      }))}
+      renderItem={({ item: line }) => <PlayLine {...line} />}
+      renderSectionHeader={({ section: { title: player } }) => (
+        <PlayLineHeader player={player} colour={colourByPlayer[player]} />
+      )}
+      keyExtractor={item => item.id.toString()}
+    />
+  )
 );
