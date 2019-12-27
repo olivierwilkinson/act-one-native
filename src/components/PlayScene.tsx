c924db623337b774bc433d20f40786fba8c0ab66
olivierwilkinson <olivier.wilkinson@gmail.com>
2019-12-27 19:54:28 +0000

    Make play background whitesmoke colour

--- src/components/PlayScene.tsx
+++ src/components/PlayScene.tsx
@@ -5,6 +5,7 @@ import PlayLine from "./PlayLine";
 import PlayLineHeader from "./PlayLineHeader";
 import { Scene, Line } from "../types/play-types";
 import { ColourByPlayer } from "../types/colour-types";
+import { playBackgroundColour } from "../styles/colours";
 
 type Props = Scene & {
   colourByPlayer: ColourByPlayer;
@@ -19,6 +20,7 @@ export default forwardRef<Component<{ sections: ListData }>, Props>(
   ({ lines, colourByPlayer }: Props, ref) => (
     <SectionList
       ref={ref}
+      style={{ backgroundColor: playBackgroundColour }}
       sections={lines.map(line => ({
         data: [line],
         title: line.player
--- src/styles/colours.ts
+++ src/styles/colours.ts
@@ -1,3 +1,4 @@
 export const primaryColour = "rgb(1, 187, 211)";
 export const lightPrimaryColour = "rgb(0, 211, 234)";
 export const secondaryColour = "rgb(82, 204, 200)";
+export const playBackgroundColour = "whitesmoke";
