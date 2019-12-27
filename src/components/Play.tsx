9bed6035e524fda2c54ce80d692bbbc357e8ee37
olivierwilkinson <olivier.wilkinson@gmail.com>
2019-12-27 14:37:35 +0000

    Update Play to render one scene at a time

--- src/components/Play.tsx
+++ src/components/Play.tsx
@@ -1,18 +1,15 @@
-import React, { useState, useRef } from "react";
-import { SectionList } from "react-native";
+import React, { useState, useRef, useEffect, useCallback } from "react";
 import palette from "google-palette";
 import convert from "color-convert";
 
 import PlayScene from "./PlayScene";
 import PlaySceneHeader from "./PlaySceneHeader";
-import { Play } from "../types/play-types";
+import { Play as PlayType, Scene } from "../types/play-types";
 
-const generateColourPalette = (play: Play) => {
+const generateColourPalette = (script: Scene[]) => {
   const players = Array.from(
     new Set(
-      [].concat(
-        ...play.script.map(({ lines }) => lines.map(line => line.player))
-      )
+      [].concat(...script.map(({ lines }) => lines.map(line => line.player)))
     )
   );
   const colours = palette("tol-rainbow", players.length);
@@ -26,23 +23,45 @@ const generateColourPalette = (play: Play) => {
   );
 };
 
-export default (play: Play) => {
-  const [colourByPlayer] = useState(generateColourPalette(play));
+const defaultProps = {
+  currentAct: 1,
+  currentScene: 1
+};
+
+const Play = ({ play, script, currentAct, currentScene }: PlayType) => {
+  const findCurrentScene = () =>
+    script.find(
+      ({ scene, act }) => act === currentAct && scene === currentScene
+    );
+
+  const sceneElement = useRef(null);
+  const [scene, setScene] = useState(findCurrentScene());
+  const [colourByPlayer, setColourByPlayer] = useState(
+    generateColourPalette(script)
+  );
+
+  useEffect(() => setColourByPlayer(generateColourPalette(script)), [play]);
+  useEffect(() => {
+    setScene(findCurrentScene());
+    sceneElement.current.scrollToLocation({
+      sectionIndex: 0,
+      itemIndex: 0,
+      animated: false
+    });
+  }, [currentAct, currentScene]);
 
   return (
-    <SectionList
-      initialNumToRender={play.script.length}
-      sections={play.script.map(scene => ({ data: [scene] }))}
-      renderItem={({ item: scene }) => (
-        <PlayScene {...scene} colourByPlayer={colourByPlayer} />
-      )}
-      renderSectionHeader={({
-        section: {
-          data: [scene]
-        }
-      }) => <PlaySceneHeader {...scene} />}
-      keyExtractor={item => `${item.act}-${item.scene}`}
-      stickySectionHeadersEnabled
-    />
+    <>
+      <PlaySceneHeader {...scene} />
+      <PlayScene
+        ref={sceneElement}
+        {...scene}
+        colourByPlayer={colourByPlayer}
+      />
+    </>
   );
 };
+
+Play.defaultProps = defaultProps;
+
+export default Play;
