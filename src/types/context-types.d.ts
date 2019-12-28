8ef3c9bb74b43f5dc6ff8030e272de2b8d9b6022
olivierwilkinson <olivier.wilkinson@gmail.com>
2019-12-28 15:38:35 +0000

    Ensure PlayContextValue includes Play properties

--- src/types/context-types.d.ts
+++ src/types/context-types.d.ts
@@ -1,4 +1,6 @@
-export interface PlayContextValue {
+import { Play } from "./play-types";
+
+export interface PlayContextValue extends Play {
   goToSceneSelect: () => void;
   goToNextScene?: () => void;
   goToPreviousScene?: () => void;
