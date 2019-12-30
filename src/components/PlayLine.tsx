b7a6547a92c47fb17c3c59c0e5da443e5d5bfdd2
olivierwilkinson <olivier.wilkinson@gmail.com>
2019-12-30 21:19:07 +0000

    Highlight current line

--- src/components/PlayLine.tsx
+++ src/components/PlayLine.tsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import { View, Text } from "react-native";
 import styled from "styled-components";
 
 import { Line } from "../types/play-types";
 import { italicFont, subFont, thinFont } from "../styles/typography";
+import PlayContext from "../contexts/Play";
+import { lightPrimaryColour } from "../styles/colours";
 
 const PlayLineView = styled(View)`
-  margin: 10px 0;
+  border-color: ${({ highlighted }: { highlighted: boolean }) =>
+    highlighted ? lightPrimaryColour : "transparent"};
+  border-width: ${({ highlighted }: { highlighted: boolean }) =>
+    highlighted ? "2px" : "0px"};
+  border-top-width: 0px;
+  padding: 10px 0;
 `;
 
 const LineRowView = styled(View)`
@@ -38,20 +45,23 @@ const LineRowNumberText = styled(Text)`
   text-align: center;
 `;
 
-export default ({ id, lineRows, player }: Line) => (
-  <PlayLineView>
-    {lineRows.map((lineRow, i) => (
-      <LineRowView key={`${id}-${i}`}>
-        <LineRowTextView>
-          <LineRowText direction={!player}>{lineRow.text}</LineRowText>
-        </LineRowTextView>
+export default ({ id, lineRows, player }: Line) => {
+  const { currentLineId } = useContext(PlayContext);
+  return (
+    <PlayLineView highlighted={currentLineId === id}>
+      {lineRows.map((lineRow, i) => (
+        <LineRowView key={`${id}-${i}`}>
+          <LineRowTextView>
+            <LineRowText direction={!player}>{lineRow.text}</LineRowText>
+          </LineRowTextView>
 
-        <LineNumberView>
-          {lineRow.number % 5 === 0 && (
-            <LineRowNumberText>{lineRow.number}</LineRowNumberText>
-          )}
-        </LineNumberView>
-      </LineRowView>
-    ))}
-  </PlayLineView>
-);
+          <LineNumberView>
+            {lineRow.number % 5 === 0 && (
+              <LineRowNumberText>{lineRow.number}</LineRowNumberText>
+            )}
+          </LineNumberView>
+        </LineRowView>
+      ))}
+    </PlayLineView>
+  );
+};
