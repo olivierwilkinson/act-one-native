c0887d7dad6b5f692dda80a04ba1484f3fd224fb
olivierwilkinson <olivier.wilkinson@gmail.com>
2019-12-30 11:20:59 +0000

    Extract scene select navigation to navigation helpers

--- src/helpers/navigation.ts
+++ src/helpers/navigation.ts
@@ -3,9 +3,14 @@ import { NavigationStackProp } from "react-navigation-stack";
 import { Play } from "../types/play-types";
 import { screenKey } from "../screens/Play";
 
+// Screen Navigation
 export const navigateToPlay = (navigation: NavigationStackProp, play: Play) =>
   navigation.navigate({
     routeName: "Play",
     params: { play },
     key: screenKey
   });
+
+// Modal Navigation
+export const openSceneSelect = (navigation: NavigationStackProp, play: Play) =>
+  navigation.navigate("PlaySceneSelect", play);
