a92fb38cb9076987f5f234a3922189dcc1033d7b
olivierwilkinson <olivier.wilkinson@gmail.com>
2019-12-30 11:14:22 +0000

    Rename navigation-helpers to navigation

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { NavigationStackScreenComponent } from "react-navigation-stack";
 
 import PlayList from "../components/PlayList";
-import { navigateToPlay } from "../helpers/navigation-helpers";
+import { navigateToPlay } from "../helpers/navigation";
 
 const HomeScreen: NavigationStackScreenComponent = ({ navigation }) => (
   <PlayList goToPlay={play => navigateToPlay(navigation, play)} />
