e891a7c08a99861e835d2e8677172a1dc16e5bac
olivierwilkinson <olivier.wilkinson@gmail.com>
2020-01-01 21:58:30 +0000

    Extract PlayLineRow from PlayLine

--- src/components/PlayLine.tsx
+++ src/components/PlayLine.tsx
@@ -1,11 +1,12 @@
 import React, { useContext } from "react";
-import { View, Text, TouchableHighlight } from "react-native";
+import { View, TouchableHighlight } from "react-native";
 import styled from "styled-components";
 
 import { Line } from "../types/play-types";
-import { italicFont, subFont, thinFont } from "../styles/typography";
 import PlayPositionContext from "../contexts/PlayPosition";
 import { lightPrimaryColour, playBackgroundColour } from "../styles/colours";
+
+import PlayLineRow from "./PlayLineRow";
 
 const PlayLineView = styled(View)`
   border-color: ${({ highlighted }: { highlighted: boolean }) =>
@@ -15,56 +16,18 @@ const PlayLineView = styled(View)`
   padding: 10px 0;
 `;
 
-const LineRowView = styled(View)`
-  display: flex;
-  flex-direction: row;
-  margin: 3px 0;
-`;
-
-const LineRowTextView = styled(View)`
-  display: flex;
-  flex-direction: row;
-  padding-left: 5%;
-  padding-right: 10px;
-  width: 90%;
-`;
-
-const LineRowText = styled(Text)`
-  ${({ direction }: { direction: boolean }) => direction && italicFont}
-  ${subFont}
-  flex-wrap: wrap;
-`;
-
-const LineNumberView = styled(View)`
-  width: 10%;
-`;
-
-const LineRowNumberText = styled(Text)`
-  ${thinFont}
-  text-align: center;
-`;
-
 export default ({ id, lineRows, player }: Line) => {
   const { activeLine, setActiveLineById } = useContext(PlayPositionContext);
 
   return (
     <TouchableHighlight
+      testID="play-line"
       onPress={() => setActiveLineById(id)}
       underlayColor={playBackgroundColour}
     >
       <PlayLineView highlighted={activeLine.id === id}>
         {lineRows.map((lineRow, i) => (
-          <LineRowView key={`${id}-${i}`}>
-            <LineRowTextView>
-              <LineRowText direction={!player}>{lineRow.text}</LineRowText>
-            </LineRowTextView>
-
-            <LineNumberView>
-              {lineRow.number % 5 === 0 && (
-                <LineRowNumberText>{lineRow.number}</LineRowNumberText>
-              )}
-            </LineNumberView>
-          </LineRowView>
+          <PlayLineRow key={`${id}-${i}`} italic={!player} {...lineRow} />
         ))}
       </PlayLineView>
     </TouchableHighlight>
---
+++ src/components/PlayLineRow.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { View, Text } from "react-native";
+import styled from "styled-components";
+
+import { LineRow } from "../types/play-types";
+import { italicFont, subFont, thinFont } from "../styles/typography";
+
+const LineRowView = styled(View)`
+  display: flex;
+  flex-direction: row;
+  margin: 3px 0;
+`;
+
+const LineRowTextView = styled(View)`
+  display: flex;
+  flex-direction: row;
+  padding-left: 5%;
+  padding-right: 10px;
+  width: 90%;
+`;
+
+const LineRowText = styled(Text)`
+  ${({ italic }: { italic: boolean }) => italic && italicFont}
+  ${subFont}
+  flex-wrap: wrap;
+`;
+
+const LineNumberView = styled(View)`
+  width: 10%;
+`;
+
+const LineRowNumberText = styled(Text)`
+  ${thinFont}
+  text-align: center;
+`;
+
+type Props = LineRow & {
+  italic: boolean;
+};
+
+export default ({ number, text, italic }: Props) => (
+  <LineRowView>
+    <LineRowTextView>
+      <LineRowText italic={italic}>{text}</LineRowText>
+    </LineRowTextView>
+
+    <LineNumberView>
+      {number % 5 === 0 && <LineRowNumberText>{number}</LineRowNumberText>}
+    </LineNumberView>
+  </LineRowView>
+);
