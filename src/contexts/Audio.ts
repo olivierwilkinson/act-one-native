d749e588bd99f993e808a39b068cb228921b971b
olivierwilkinson <olivier.wilkinson@gmail.com>
2020-01-01 23:43:36 +0000

    Remove unused Done playbackState option

--- src/contexts/Audio.ts
+++ src/contexts/Audio.ts
@@ -3,8 +3,7 @@ import React from "react";
 export enum PlaybackState {
   Playing = "PLAYING",
   Paused = "PAUSED",
-  Stopped = "STOPPED",
-  Done = "DONE"
+  Stopped = "STOPPED"
 }
 
 export interface AudioContextValue {
