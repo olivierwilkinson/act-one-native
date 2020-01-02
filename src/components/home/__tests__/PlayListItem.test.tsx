dca1468fafac91d5233dc97788bbec1965ed8a7a
olivierwilkinson <olivier.wilkinson@gmail.com>
2020-01-02 00:11:23 +0000

    Add PlayListItem tests

---
+++ src/components/home/__tests__/PlayListItem.test.tsx
@@ -0,0 +1,41 @@
+import "react-native";
+import React from "react";
+import {
+  render,
+  fireEvent,
+  cleanup,
+  QueryByAPI
+} from "react-native-testing-library";
+
+import PlayListItem from "../PlayListItem";
+
+import play from "../../../../tests/mocks/play";
+
+describe("PlayListItem", () => {
+  let onPress: jest.Mock;
+  let queryByTestId: QueryByAPI["queryByTestId"];
+  let queryByText: QueryByAPI["queryByText"];
+  beforeEach(() => {
+    onPress = jest.fn();
+
+    ({ queryByTestId, queryByText } = render(
+      <PlayListItem {...play} onPress={onPress} />
+    ));
+  });
+  afterEach(cleanup);
+
+  it("renders play title", () => {
+    expect(queryByText(play.play)).not.toBeNull();
+  });
+
+  it("renders play description", () => {
+    expect(queryByText(play.description)).not.toBeNull();
+  });
+
+  it("it calls onPress when pressed", () => {
+    const playListItem = queryByTestId("play-list-item");
+    fireEvent.press(playListItem);
+
+    expect(onPress).toHaveBeenCalled();
+  });
+});
