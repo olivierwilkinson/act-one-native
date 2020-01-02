b135906fd661a0bc1744b4e750e6013ed0475575
olivierwilkinson <olivier.wilkinson@gmail.com>
2020-01-02 00:26:52 +0000

    Move importing of play data to Home screen

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { NavigationStackScreenComponent } from "react-navigation-stack";
 
+import plays from "../data/plays";
 import PlayList from "../components/home/PlayList";
 import { navigateToPlay } from "../helpers/navigation";
 
 const HomeScreen: NavigationStackScreenComponent = ({ navigation }) => (
-  <PlayList goToPlay={play => navigateToPlay(navigation, play)} />
+  <PlayList plays={plays} goToPlay={play => navigateToPlay(navigation, play)} />
 );
 
 export default HomeScreen;
