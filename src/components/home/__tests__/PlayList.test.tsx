d43cccd2743156ec9115d82147e6b3bfe500a1b4
olivierwilkinson <olivier.wilkinson@gmail.com>
2020-01-04 19:51:21 +0000

    Add test for goToPlay prop in PlayList

--- src/components/home/__tests__/PlayList.test.tsx
+++ src/components/home/__tests__/PlayList.test.tsx
@@ -1,6 +1,12 @@
 import "react-native";
 import React from "react";
-import { render, cleanup, QueryByAPI } from "react-native-testing-library";
+import {
+  render,
+  cleanup,
+  fireEvent,
+  QueryByAPI,
+  GetByAPI
+} from "react-native-testing-library";
 
 import PlayList from "../PlayList";
 import play from "../../../data/plays/shakespeare/AComedyOfErrors";
@@ -8,14 +14,24 @@ import play from "../../../data/plays/shakespeare/AComedyOfErrors";
 describe("PlayList", () => {
   let goToPlay: jest.Mock;
   let queryByText: QueryByAPI["queryByText"];
+  let getByTestId: GetByAPI["getByTestId"];
   beforeEach(() => {
     goToPlay = jest.fn();
 
-    ({ queryByText } = render(<PlayList plays={[play]} goToPlay={goToPlay} />));
+    ({ queryByText, getByTestId } = render(
+      <PlayList plays={[play]} goToPlay={goToPlay} />
+    ));
   });
   afterEach(cleanup);
 
   it("renders play list item", () => {
     expect(queryByText(play.play)).not.toBeNull();
+  });
+
+  it("calls goToPlay with play on list item press", () => {
+    const playListItem = getByTestId("play-list-item");
+    fireEvent.press(playListItem);
+
+    expect(goToPlay).toHaveBeenCalledWith(play);
   });
 });
