6948d3c30076d4bbdbdcb467ff6045ef3ba25e32
olivierwilkinson <olivier.wilkinson@gmail.com>
2020-01-05 16:16:23 +0000

    Add PlaySceneSelectModal tests

---
+++ src/screens/__tests__/PlaySceneSelectModal.test.tsx
@@ -0,0 +1,40 @@
+import "react-native";
+import React from "react";
+import { render, cleanup, QueryByAPI } from "react-native-testing-library";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import "react-native-reanimated";
+
+jest.mock("react-native/Libraries/Animated/src/NativeAnimatedHelper");
+
+import play from "../../data/plays/shakespeare/AComedyOfErrors";
+import PlaySceneSelectModal from "../PlaySceneSelectModal";
+
+describe("PlayScreen", () => {
+  let queryByText: QueryByAPI["queryByText"];
+
+  beforeEach(() => {
+    const PlayContainer = createAppContainer(
+      createStackNavigator(
+        {
+          PlaySceneSelectModal: { screen: PlaySceneSelectModal }
+        },
+        {
+          initialRouteName: "PlaySceneSelectModal",
+          initialRouteParams: { play }
+        }
+      )
+    );
+
+    ({ queryByText } = render(<PlayContainer />));
+  });
+  afterEach(cleanup);
+
+  it("renders cancel button on header", () => {
+    expect(queryByText("Cancel")).not.toBeNull();
+  });
+
+  it("renders correct header title", () => {
+    expect(queryByText(play.play)).not.toBeNull();
+  });
+});
