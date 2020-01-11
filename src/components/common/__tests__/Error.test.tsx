6bec7c8b3a8a46e340241a0e936d787c8358e9a4
olivierwilkinson <olivier.wilkinson@gmail.com>
2020-01-11 10:57:16 +0000

    Add Error common component

---
+++ src/components/common/__tests__/Error.test.tsx
@@ -0,0 +1,19 @@
+import "react-native";
+import React from "react";
+import { render, cleanup, QueryByAPI } from "react-native-testing-library";
+
+import Error from "../Error";
+
+describe("Header", () => {
+  let message: string;
+  let queryByText: QueryByAPI["queryByText"];
+  beforeEach(() => {
+    message = "Test error message";
+    ({ queryByText } = render(<Error message={message} />));
+  });
+  afterEach(cleanup);
+
+  it("renders message", () => {
+    expect(queryByText(message)).not.toBeNull();
+  });
+});
