7c2aa59cc6de724bf1af849c074cf6e05e97e602
olivierwilkinson <olivier.wilkinson@gmail.com>
2020-01-11 11:10:48 +0000

    Move createPlayNavigation to contexts helpers

---
+++ src/helpers/contexts.ts
@@ -0,0 +1,28 @@
+import { NavigationStackProp } from "react-navigation-stack";
+
+import { Play } from "../types/play-types";
+import { findActiveScene, goToScene } from "../helpers/play";
+import { openSceneSelect } from "../helpers/navigation";
+
+export const createPlayNavigation = (
+  navigation: NavigationStackProp,
+  play: Play
+) => {
+  const { scenes } = play;
+  const activeScene = findActiveScene(play);
+  const sceneIndex = scenes.indexOf(activeScene!);
+
+  const goToNextScene =
+    scenes[sceneIndex + 1] &&
+    (() => goToScene(navigation, play, sceneIndex + 1));
+
+  const goToPreviousScene =
+    scenes[sceneIndex - 1] &&
+    (() => goToScene(navigation, play, sceneIndex - 1));
+
+  return {
+    goToNextScene,
+    goToPreviousScene,
+    openSceneSelect: openSceneSelect.bind(null, navigation, play)
+  };
+};
