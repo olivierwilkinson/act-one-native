fe299f4b8f597e55d827a9eeb9d71b568f1f12bb
olivierwilkinson <olivier.wilkinson@gmail.com>
2020-02-22 14:45:18 +0000

    Add async storage helpers for play settings

---
+++ src/helpers/storage.ts
@@ -0,0 +1,33 @@
+import "react-native";
+
+import { AsyncStorage } from "react-native";
+
+import { Play } from "../types/play-types";
+import { PlaySettings } from "../contexts/PlaySettings";
+
+export const getStoredSettings: (
+  play: Play
+) => Promise<null | PlaySettings> = async play => {
+  try {
+    const serialisedSettings = await AsyncStorage.getItem(
+      `@${play.play}-settings`
+    );
+
+    if (!serialisedSettings) {
+      return null;
+    }
+
+    return JSON.parse(serialisedSettings);
+  } catch (e) {
+    return null;
+  }
+};
+
+export const setStoredSettings = async (play: Play, settings: PlaySettings) => {
+  try {
+    const serialisedSettings = JSON.stringify(settings);
+    await AsyncStorage.setItem(`@${play.play}-settings`, serialisedSettings);
+  } catch (e) {
+    return;
+  }
+};
