a62417dd2d0ecec27edef7deb98839e560c7a08a
olivierwilkinson <olivier.wilkinson@gmail.com>
2020-04-08 19:35:51 +0100

    Update PlayScreen tests and refactor didUpdate

--- src/screens/__tests__/PlayScreen.test.tsx
+++ src/screens/__tests__/PlayScreen.test.tsx
@@ -6,8 +6,12 @@ import {
   QueryByAPI,
   flushMicrotasksQueue
 } from "react-native-testing-library";
-import { createAppContainer } from "react-navigation";
+import {
+  createAppContainer,
+  NavigationContainerComponent
+} from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
+import { NavigationActions } from "react-navigation";
 import "react-native-reanimated";
 
 import { getStoredSettings, setStoredSettings } from "../../helpers/storage";
@@ -24,7 +28,7 @@ jest.mock("../../helpers/storage.ts", () => ({
 const getStoredSettingsMock = getStoredSettings as jest.Mock;
 const setStoredSettingsMock = setStoredSettings as jest.Mock;
 
-const createPlayContainer = (params: Partial<Params>) =>
+const createPlayContainer = (params?: Params) =>
   createAppContainer(
     createStackNavigator(
       {
@@ -32,47 +36,125 @@ const createPlayContainer = (params: Partial<Params>) =>
       },
       {
         initialRouteName: "Play",
+        initialRouteKey: "play-screen",
         initialRouteParams: params
       }
     )
   );
 
+// NavigationService.js
+let navigator: NavigationContainerComponent;
+
+function setParams(params: Params) {
+  navigator.dispatch(
+    NavigationActions.navigate({
+      routeName: "Play",
+      key: "play-screen",
+      params
+    })
+  );
+}
+
 describe("PlayScreen", () => {
   let queryByText: QueryByAPI["queryByText"];
 
-  beforeEach(async () => {
-    const PlayContainer = createPlayContainer({ play });
-    ({ queryByText } = render(<PlayContainer />));
-
-    await flushMicrotasksQueue();
-  });
-  afterEach(cleanup);
-
-  it("renders back button on header", () => {
-    expect(queryByText("Back")).not.toBeNull();
+  afterEach(() => {
+    cleanup();
+    getStoredSettingsMock.mockRestore();
+    setStoredSettingsMock.mockRestore();
   });
 
-  it("renders correct header title", () => {
-    expect(queryByText(play.play)).not.toBeNull();
-  });
-
-  it("calls getStoredSettings on mount", () => {
-    expect(getStoredSettingsMock).toHaveBeenCalled();
-  });
-
-  describe("when passed play and settings", () => {
-    let settings: PlaySettings;
+  describe("when mounted without params", () => {
     beforeEach(async () => {
-      settings = {
-        selectedPlayer: "some character"
-      };
-
-      const PlayContainer = createPlayContainer({ play, settings });
-      ({ queryByText } = render(<PlayContainer />));
+      const PlayContainer = createPlayContainer();
+      ({ queryByText } = render(
+        <PlayContainer
+          ref={nav => {
+            if (nav) {
+              navigator = nav;
+            }
+          }}
+        />
+      ));
     });
 
-    it.skip("calls setStoredSettings", () => {
-      expect(setStoredSettingsMock).toHaveBeenCalledWith(settings);
+    it("renders back button on header", () => {
+      expect(queryByText("Back")).not.toBeNull();
+    });
+
+    it("renders default header", () => {
+      expect(queryByText("ActOne")).not.toBeNull();
+    });
+
+    it("renders error fallback", () => {
+      expect(queryByText("Play could not be loaded")).not.toBeNull();
+    });
+  });
+
+  describe("when mounted with play", () => {
+    beforeEach(() => {
+      const PlayContainer = createPlayContainer({ play });
+      ({ queryByText } = render(
+        <PlayContainer
+          ref={nav => {
+            if (nav) {
+              navigator = nav;
+            }
+          }}
+        />
+      ));
+    });
+
+    it("renders correct header title", () => {
+      expect(queryByText(play.play)).not.toBeNull();
+    });
+
+    it("calls getStoredSettings on mount", () => {
+      expect(getStoredSettingsMock).toHaveBeenCalled();
+    });
+
+    it("renders loading indicator", () => {
+      expect(queryByText(`Loading ${play.play}...`)).not.toBeNull();
+    });
+
+    describe("when finished loading", () => {
+      beforeEach(async () => {
+        await flushMicrotasksQueue();
+      });
+
+      it("sets settings as an empty object", () => {
+        expect(setStoredSettingsMock).toHaveBeenCalledWith(play, {});
+      });
+
+      it("renders play", () => {
+        expect(
+          queryByText(play.scenes[0].lines[0].lineRows[0].text)
+        ).not.toBeNull();
+      });
+
+      describe("when passed settings", () => {
+        let settings: PlaySettings;
+        beforeEach(async () => {
+          settings = {
+            act: 1,
+            scene: 2
+          };
+
+          setParams({ play, settings });
+
+          await flushMicrotasksQueue();
+        });
+
+        it("calls setStoredSettings", () => {
+          expect(setStoredSettingsMock).toHaveBeenCalledWith(play, settings);
+        });
+
+        it("renders new scene", () => {
+          expect(
+            queryByText(play.scenes[1].lines[0].lineRows[0].text)
+          ).not.toBeNull();
+        });
+      });
     });
   });
 });
