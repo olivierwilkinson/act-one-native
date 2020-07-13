103bdf2ad419a7d95bb530643edc323720580b5d
olivierwilkinson <olivier.wilkinson@gmail.com>
2020-07-13 21:06:55 +0100

    Add SceneContainer

--- src/components/play/Play.tsx
+++ src/components/play/Play.tsx
@@ -1,16 +1,13 @@
-import React, { useContext } from "react";
+import React from "react";
 
-import Scene from "./scene/Scene";
+import SceneContainer from "./scene/SceneContainer";
 
 import { Play as PlayType } from "../../types/play-types";
-import PlayPositionContext from "../../contexts/PlayPosition";
 
 type Props = {
   play: PlayType;
 };
 
-export default ({ play: { colourByPlayer } }: Props) => {
-  const { activeScene } = useContext(PlayPositionContext);
-
-  return <Scene {...activeScene} colourByPlayer={colourByPlayer} />;
-};
+export default ({ play: { colourByPlayer } }: Props) => (
+  <SceneContainer colourByPlayer={colourByPlayer} />
+);
