42c4136a4c145f3cd498b1e9e581fa26fd1c13bd
olivierwilkinson <olivier.wilkinson@gmail.com>
2020-08-01 17:12:19 +0100

    Add LoginScreen

---
+++ src/screens/LoginScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View } from "react-native";
+
+import GoogleSignInButtonContainer from "../components/login/googleSignInButton/GoogleSignInButtonContainer";
+
+export default function Login() {
+  return (
+    <View>
+      <GoogleSignInButtonContainer />
+    </View>
+  );
+}
