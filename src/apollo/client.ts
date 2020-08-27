e8095837f7c267e83b2bfdd0ddf26a52aafab425
olivierwilkinson <olivier.wilkinson@gmail.com>
2020-08-27 22:47:50 +0100

    Update apollo client to work with nexus

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,6 +1,5 @@
 import { ApolloClient, createHttpLink } from "@apollo/client";
 import { InMemoryCache } from "@apollo/client/cache";
-import { BatchHttpLink } from "@apollo/client/link/batch-http";
 import Constants from "expo-constants";
 
 import fetch from "../helpers/fetch";
@@ -8,11 +7,8 @@ import fetch from "../helpers/fetch";
 const { apiBaseUrl = "" } = Constants.manifest.extra || {};
 const uri = `${apiBaseUrl}/graphql`;
 
-const batchHttpLink = new BatchHttpLink({ uri, fetch });
-const httpLink = createHttpLink({ uri, fetch });
-
-export default ({ batchHttpRequests = true } = {}) =>
+export default () =>
   new ApolloClient({
-    link: batchHttpRequests ? batchHttpLink : httpLink,
+    link: createHttpLink({ uri, fetch, useGETForQueries: true }),
     cache: new InMemoryCache()
   });
