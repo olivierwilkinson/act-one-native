0f4ace5d6d03c88cc1e3dee5c4eddaa94947ca4f
olivierwilkinson <olivier.wilkinson@gmail.com>
2020-09-01 09:17:59 +0100

    Don't spin up backend for sync-gql command

--- scripts/gql/get-gql-schema.sh
+++ scripts/gql/get-gql-schema.sh
@@ -2,11 +2,6 @@
 
 SCHEMA_FILE=${1:-"graphql-schema.json"}
 
-source "scripts/helpers.sh"
-
-trap stop_services EXIT
-
-start_services &&
 yarn run apollo schema:download \
   --endpoint=http://localhost:8000/graphql \
   $SCHEMA_FILE
