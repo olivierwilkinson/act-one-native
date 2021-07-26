d360f4e9d204c921008b376e4297de902892f40a
olivierwilkinson <olivier.wilkinson@gmail.com>
2021-07-26 19:59:21 -0400

    Add typePolicies for basic queries

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -6,5 +6,36 @@ import { apiBaseUrl } from "../services/baseUrls";
 export default () =>
   new ApolloClient({
     link: createHttpLink({ fetch, uri: `${apiBaseUrl}/graphql` }),
-    cache: new InMemoryCache(),
+    cache: new InMemoryCache({
+      typePolicies: {
+        Query: {
+          fields: {
+            play(_, { args, toReference }) {
+              return toReference({
+                __typename: "Play",
+                id: args?.where.id,
+              });
+            },
+            scene(_, { args, toReference }) {
+              return toReference({
+                __typename: "Scene",
+                id: args?.where.id,
+              });
+            },
+            line(_, { args, toReference }) {
+              return toReference({
+                __typename: "Line",
+                id: args?.where.id,
+              });
+            },
+            lineRow(_, { args, toReference }) {
+              return toReference({
+                __typename: "LineRow",
+                id: args?.where.id,
+              });
+            },
+          },
+        },
+      },
+    }),
   });
